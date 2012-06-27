bf487729dedf877739e9ba88ff59f95a4409939b
Leandro Guimaraes <leguimas@gmail.com>
2012-06-27 10:09:21 -0300

    A better funcional dashboard. Thanks a lot @erickzanardo.

--- view/main-web/src/main/webapp/js/muuk/dashboard.js
+++ view/main-web/src/main/webapp/js/muuk/dashboard.js
@@ -2,28 +2,39 @@
 Muuk.dashboard = {};
 
 Muuk.dashboard.load = function() {
-	Muuk.dashboard.loadOpportunities('Refinamento');
-	Muuk.dashboard.loadOpportunities('OrdemGrandeza');
-	Muuk.dashboard.loadOpportunities('VisitaTecnica');
-	Muuk.dashboard.loadOpportunities('AguardandoCliente');
-	Muuk.dashboard.loadOpportunities('EmElaboracao');
-	Muuk.dashboard.loadOpportunities('Defesa');
-	Muuk.dashboard.loadOpportunities('NegociacaoAlta');
-	Muuk.dashboard.loadOpportunities('NegociacaoMedia');
-	Muuk.dashboard.loadOpportunities('NegociacaoBaixa');
-	Muuk.dashboard.loadOpportunities('Ganhamos');
-	Muuk.dashboard.loadOpportunities('Perdemos');
-	Muuk.dashboard.loadOpportunities('OnHold');
-	Muuk.dashboard.loadOpportunities('AguardandoTimeTecnico');
+	Muuk.dashboard.loadOpportunities('Refinamento', 'Refinamento', 'section.refinamento');
+	Muuk.dashboard.loadOpportunities('OrdemGrandeza', 'Ordem de Grandeza', 'section.ordemGrandeza');
+	Muuk.dashboard.loadOpportunities('VisitaTecnica', 'Visita Técnica', 'section.visitaTecnica');
+	Muuk.dashboard.loadOpportunities('AguardandoCliente', 'Aguardando Cliente', 'section.aguardandoCliente');
+	Muuk.dashboard.loadOpportunities('EmElaboracao', 'Em Elaboração', 'section.emElaboracao');
+	Muuk.dashboard.loadOpportunities('Defesa', 'Defesa', 'section.defesa');
+	Muuk.dashboard.loadOpportunities('NegociacaoAlta', 'Alta Probabilidade', 'section.negociacaoAlta');
+	Muuk.dashboard.loadOpportunities('NegociacaoMedia', 'Média Probabilidade', 'section.negociacaoMedia');
+	Muuk.dashboard.loadOpportunities('NegociacaoBaixa', 'Baixa Probabilidade', 'section.negociacaoBaixa');
+	Muuk.dashboard.loadOpportunities('Ganhamos', 'Ganhamos', 'section.ganhamos');
+	Muuk.dashboard.loadOpportunities('Perdemos', 'Perdemos', 'section.perdemos');
+	Muuk.dashboard.loadOpportunities('OnHold', 'On Hold', 'section.onHold');
+	Muuk.dashboard.loadOpportunitiesAguardandoTimeTecnico();
 }
 
-Muuk.dashboard.loadOpportunities = function(status) {
+Muuk.dashboard.loadOpportunities = function(statusCode, statusName, seletor) {
 	$.ajax({
 		type : 'GET',
-		url : 's/opportunity/all/' + status,
+		url : 's/opportunity/all/' + statusCode,
 		dataType : 'json',
 		success : function(opportunities) {
-			$.holy("templates/opportunities/" + status + ".xml", { 'opportunities' : opportunities });
+			$.holy("templates/opportunities/opportunities.xml", { 'opportunities' : opportunities, 'status' : statusName, 'seletor' : seletor });
+		}
+	});
+}
+
+Muuk.dashboard.loadOpportunitiesAguardandoTimeTecnico = function() {
+	$.ajax({
+		type : 'GET',
+		url : 's/opportunity/all/AguardandoTimeTecnico',
+		dataType : 'json',
+		success : function(opportunities) {
+			$.holy("templates/opportunities/aguardandoTimeTecnico.xml", { 'opportunities' : opportunities });
 		}
 	});
 }
