fc42ce31b576c087e00fa019583ad5b50ebbaf81
Leandro Guimarães <leguimas@gmail.com>
2013-01-28 09:01:19 -0200

    Atualizando novos códigos de status das propostas

--- view/main-web/src/main/webapp/js/muuk/dashboard.js
+++ view/main-web/src/main/webapp/js/muuk/dashboard.js
@@ -4,18 +4,18 @@ Muuk.dashboard = {};
 Muuk.dashboard.MAX_OPPORTUNITIES = 10;
 
 Muuk.dashboard.load = function() {
-	Muuk.dashboard.loadOpportunities('05_Refinamento', 'Refinamento', 'section.refinamento');
-	Muuk.dashboard.loadOpportunities('06_OrdemGrandeza', 'Ordem de Grandeza', 'section.ordemGrandeza');
-	Muuk.dashboard.loadOpportunities('07_VisitaTecnica', 'Visita Técnica', 'section.visitaTecnica');
-	Muuk.dashboard.loadOpportunities('09_AguardandoCliente', 'Aguardando Cliente', 'section.aguardandoCliente');
-	Muuk.dashboard.loadOpportunities('10_EmElaboracao', 'Em Elaboração', 'section.emElaboracao');
-	Muuk.dashboard.loadOpportunities('11_Defesa', 'Defesa', 'section.defesa');
-	Muuk.dashboard.loadOpportunities('12_NegociacaoAlta', 'Alta Probabilidade', 'section.negociacaoAlta');
-	Muuk.dashboard.loadOpportunities('13_NegociacaoMedia', 'Média Probabilidade', 'section.negociacaoMedia');
-	Muuk.dashboard.loadOpportunities('14_NegociacaoBaixa', 'Baixa Probabilidade', 'section.negociacaoBaixa');
-	Muuk.dashboard.loadYearOpportunities('15_Ganhamos', 'Ganhamos', 'section.ganhamos');
-	Muuk.dashboard.loadYearOpportunities('16_Perdemos', 'Perdemos', 'section.perdemos');
-	Muuk.dashboard.loadYearOpportunities('17_OnHold', 'On Hold', 'section.onHold');
+	Muuk.dashboard.loadOpportunities('06_Refinamento', 'Refinamento', 'section.refinamento');
+	Muuk.dashboard.loadOpportunities('07_OrdemGrandeza', 'Ordem de Grandeza', 'section.ordemGrandeza');
+	Muuk.dashboard.loadOpportunities('08_VisitaTecnica', 'Visita Técnica', 'section.visitaTecnica');
+	Muuk.dashboard.loadOpportunities('10_AguardandoCliente', 'Aguardando Cliente', 'section.aguardandoCliente');
+	Muuk.dashboard.loadOpportunities('12_EmElaboracao', 'Em Elaboração', 'section.emElaboracao');
+	Muuk.dashboard.loadOpportunities('13_Defesa', 'Defesa', 'section.defesa');
+	Muuk.dashboard.loadOpportunities('15_NegociacaoAlta', 'Alta Probabilidade', 'section.negociacaoAlta');
+	Muuk.dashboard.loadOpportunities('16_NegociacaoMedia', 'Média Probabilidade', 'section.negociacaoMedia');
+	Muuk.dashboard.loadOpportunities('17_NegociacaoBaixa', 'Baixa Probabilidade', 'section.negociacaoBaixa');
+	Muuk.dashboard.loadYearOpportunities('18_Ganhamos', 'Ganhamos', 'section.ganhamos');
+	Muuk.dashboard.loadYearOpportunities('19_Perdemos', 'Perdemos', 'section.perdemos');
+	Muuk.dashboard.loadYearOpportunities('20_OnHold', 'On Hold', 'section.onHold');
 	Muuk.dashboard.loadOpportunitiesAguardandoTimeTecnico();
 }
 
@@ -46,7 +46,7 @@ Muuk.dashboard.loadOpportunities = function(statusCode, statusName, seletor) {
 Muuk.dashboard.loadOpportunitiesAguardandoTimeTecnico = function() {
 	$.ajax({
 		type : 'GET',
-		url : 's/opportunity/all/08_AguardandoTimeTecnico',
+		url : 's/opportunity/all/09_AguardandoTimeTecnico',
 		dataType : 'json',
 		success : function(opportunities) {
 			$.holy("templates/opportunities/aguardandoTimeTecnico.xml?t=" + new Date().getTime(), { 'opportunities' : opportunities });
